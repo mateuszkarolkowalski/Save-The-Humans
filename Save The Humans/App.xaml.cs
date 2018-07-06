e43ab8e4e4c327df32d583a7cb180aeb3bbbf78a
Mateusz Kowalski <mateusz.karol.kowalski@gmail.com>
2018-07-06 18:35:46 +0200

    Dodaj pliki projektów.

---
+++ Save The Humans/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Save_The_Humans
+{
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
